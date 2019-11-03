36a396a53f4dcb955c012f49b4ce79db6381a1f8
someone-1 <nagesh.srinivas@timeinc.com>
2019-11-03 22:10:02 +0530

    mvn project

---
+++ src/main/java/Db/Page.java
@@ -0,0 +1,24 @@
+package Db;
+
+public class Page implements Constants {
+
+
+    public Page(){
+
+
+    }
+
+    private StringBuilder s ;
+
+
+
+    public Page getPage(int id){
+
+
+        return null;
+    }
+
+
+
+
+}
---
+++ src/main/java/Db/Utils.java
@@ -0,0 +1,14 @@
+package Db;
+
+public interface Utils {
+
+    public int pageSize = 16000;
+
+    public static String padding(int val, int digits){
+        // if digits exceeds in int exceeds value of digits
+        // throw exception
+        String strFormat = "%0" + String.valueOf(digits) + "d";
+        return String.format(strFormat, val);
+    }
+
+}
