495286f02209d0acdddb99c1561d0e25c92b0659
someone-1 <nagesh.srinivas@timeinc.com>
2020-02-05 23:35:30 +0530

    using bytes instead of string

--- src/main/java/Db/Page.java
+++ src/main/java/Db/Page.java
@@ -1,15 +1,32 @@
 package Db;
 
+import java.util.HashMap;
+
 public class Page implements Utils {
+
+    HashMap<String, Integer> pageHeader;
 
 
     public Page(){
+
+        byte[] page = new byte[Utils.pageSize];
+
 
         StringBuilder page = new StringBuilder(Utils.pageSize);
 
 
 
     }
+    public Page(byte[] pageInByte){
+
+
+    }
+
+    public byte[] convertPageToBytes(){
+
+        return null;
+    }
+
 
     private StringBuilder s ;
 
--- src/main/java/Db/Utils.java
+++ src/main/java/Db/Utils.java
@@ -5,6 +5,19 @@ public interface Utils {
     public int pageSize = 100;
 
 
+    public static byte[] stringToByte(String str, int lenght){
+        // support other encoding
+        // if ascii value exceeds lenght throw exception
+
+        return null;
+
+    }
+
+    public static byte[] intToByte(int integer , int lenght){
+
+        // if byte value exceeds lenght throw exception
+        return null;
+    }
 
     public static String padding(int val, int digits){
         // if digits exceeds in int exceeds value of digits
