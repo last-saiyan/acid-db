87c6f858992e02b27db8ee7661b931c3557c3ce5
someone-1 <nagesh.srinivas@timeinc.com>
2020-05-26 23:53:04 +0530

    started deadlock detector

--- src/main/java/Db/Tx/LockTable.java
+++ src/main/java/Db/Tx/LockTable.java
@@ -7,23 +7,25 @@ package Db.Tx;
 * locks page in shared mode or exclusive mode
 * */
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class LockTable {
 
 //    map of exclusive locks pageID to transactionID
-    private HashMap<Integer, Integer> PIDToTIDExclusiveLock;
+    private HashMap<Integer, Integer> PIDExclusiveLock;
 
 //    map of shared locks pageID to transactionID set
-    private HashMap<Integer, Set<Integer>> PIDToTIDSharedLock;
+    private HashMap<Integer, Set<Integer>> PIDSharedLock;
+
+//    map of transaction id that is waiting to lock page id
+
+    private HashMap<Integer, Integer> TIDWaitingforSLocks;
+    private HashMap<Integer, Integer> TIDWaitingforXLocks;
 
 
     public LockTable(){
-        PIDToTIDExclusiveLock = new HashMap<>();
-        PIDToTIDSharedLock = new HashMap<>();
+        PIDExclusiveLock = new HashMap<>();
+        PIDSharedLock = new HashMap<>();
     }
 
 
@@ -35,45 +37,108 @@ public class LockTable {
     * */
     public synchronized boolean grantLock(int pageID, int transactionID, Permission perm){
 
-//        check if page has exclusive lock
-        if (PIDToTIDExclusiveLock.containsKey(pageID)){
-//                wait till the other transaction is completed
+//        if same transaction holds exclusive lock on page return true else false
+        if (PIDExclusiveLock.containsKey(pageID)){
+
+            if(PIDExclusiveLock.get(pageID) == transactionID){
+                return true;
+            }
+            addToWaitingList(transactionID, pageID, perm);
+//            wait till the other transaction is completed
             System.out.println("page - " +  pageID + " is held by exclusive lock by transaction - " + transactionID);
             return false;
         }
 
         if(perm == Permission.SHARED){
             Set<Integer> TID;
-            if (PIDToTIDSharedLock.containsKey(pageID)){
-                TID = PIDToTIDSharedLock.get(pageID);
+            if (PIDSharedLock.containsKey(pageID)){
+                TID = PIDSharedLock.get(pageID);
                 if(!TID.contains(transactionID)){
                     TID.add(transactionID);
                 }
-                PIDToTIDSharedLock.put(pageID, TID);
+                PIDSharedLock.put(pageID, TID);
             }else {
                 TID = new HashSet<>();
                 TID.add(pageID);
-                PIDToTIDSharedLock.put(pageID, TID);
+                PIDSharedLock.put(pageID, TID);
             }
+            removeFromWaitingList(transactionID, perm);
             return true;
-
         }else {
 
-            if(PIDToTIDSharedLock.containsKey(pageID)){
+            if(PIDSharedLock.containsKey(pageID)){
 //                wait till the other transaction is completed
                 System.out.println("page - " +  pageID + " is held by shared lock by transaction - " + transactionID);
-
+                addToWaitingList(transactionID, pageID, perm);
                 return false;
             }
-            PIDToTIDExclusiveLock.put(pageID, transactionID);
+            PIDExclusiveLock.put(pageID, transactionID);
+            removeFromWaitingList(transactionID, perm);
             return true;
         }
     }
 
 
+    /*
+    *
+    * */
+    private void addToWaitingList(int transactionID, int pageID, Permission perm){
+        if(perm == Permission.SHARED){
+            TIDWaitingforSLocks.put(transactionID, pageID);
+        }else {
+            TIDWaitingforXLocks.put(transactionID, pageID);
+        }
+    }
+
 
     /*
-    * to be called after the transaction commits
+    * if transaction is present
+    * */
+    private void removeFromWaitingList(int transactionID, Permission perm){
+        if(perm == Permission.SHARED){
+            if(TIDWaitingforSLocks.containsKey(transactionID)){
+                TIDWaitingforSLocks.remove(transactionID);
+            }
+        }else {
+            if(TIDWaitingforXLocks.containsKey(transactionID)){
+                TIDWaitingforXLocks.remove(transactionID);
+            }
+        }
+    }
+
+
+    //            get the pageid that has to be locked
+//            check if other transaction is waiting for this
+//            and check if other is transaction is also waiting for lock on a page
+//            held by this transactionID
+
+    public boolean detectDeadLock(int transactionID){
+
+
+
+        return false;
+    }
+
+    boolean transactionHoldsLock(int transactionID, int pageID){
+
+        return false;
+    }
+
+
+    /*
+    * this is returns the transactionID that holds the
+    * exclusive lock on the given pageID
+    * */
+    public int getTransactionID(int pageID){
+        if (PIDExclusiveLock.containsKey(pageID)){
+            return PIDExclusiveLock.get(pageID);
+        }
+        return -1;
+    }
+
+
+    /*
+    * to be called after the transaction commits or aborts
     * following 2PL
     *
     * */
@@ -83,12 +148,17 @@ public class LockTable {
         Set<Integer> transactionIDSet;
         if (pageIter.hasNext()){
             tempPageID = pageIter.next();
-            PIDToTIDExclusiveLock.remove(tempPageID);
+            PIDExclusiveLock.remove(tempPageID);
 //            shared lock - remove transactionID from set
-            transactionIDSet = PIDToTIDSharedLock.get(pageID);
+            transactionIDSet = PIDSharedLock.get(pageID);
             transactionIDSet.remove(transactionID);
-            PIDToTIDSharedLock.put(tempPageID, transactionIDSet);
+            PIDSharedLock.put(tempPageID, transactionIDSet);
         }
+
+//        this is called to free up map, if the transaction is aborted
+        removeFromWaitingList(transactionID, Permission.SHARED);
+        removeFromWaitingList(transactionID, Permission.EXCLUSIVE);
+
     }
 
 
--- src/main/java/Db/Tx/Transaction.java
+++ src/main/java/Db/Tx/Transaction.java
@@ -1,6 +1,7 @@
 package Db.Tx;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 /*
@@ -13,22 +14,52 @@ import java.util.Set;
 public class Transaction {
 
     private static int tID = 0;
-    private static LockTable locks = new LockTable();
+    private static LockTable lockManager = new LockTable();
     private Set<Integer> pagesLocked;
 
 
     public Transaction(){
-
-
+        pagesLocked = new HashSet<>();
         incrementID();
     }
 
 
 
-    public synchronized void lockPage(int pageID, Permission perm){
+    /*
+    * acquires lock for a page
+    * if lock is not available
+    * sleep the thread for some time
+    * if lock is not acquired within the timeout
+    * check for deadlock ?
+    * abort the transaction
+    * */
+    public synchronized void lockPage(int pageID, Permission perm) throws InterruptedException {
 
+        if(!lockManager.grantLock(pageID, tID, perm)) {
+            int timeout = 5000;
+            Thread.sleep(timeout);
+
+            if(lockManager.grantLock(pageID, tID, perm)){
+                pagesLocked.add(pageID);
+                return;
+            }else {
+//                check for deadlocks
+                if(detectDeadLocks()){
+                    abort();
+                }
+            }
+        }else{
+            pagesLocked.add(pageID);
+        }
     }
 
+
+    /*
+    * checks if there is a cycle in graph
+    * */
+    private boolean detectDeadLocks(){
+        return lockManager.detectDeadLock(tID);
+    }
 
 
     public int getTID(){
@@ -49,18 +80,20 @@ public class Transaction {
     * */
 
     public void releaseAllLocks(){
-
+        lockManager.releaseAllLock(tID, pagesLocked);
     }
 
 
     public void commit(){
-
+//        need to do more work here
+        releaseAllLocks();
     }
 
 
 
     public void abort(){
-
+//        need to do more work here
+        releaseAllLocks();
     }
 
 }
